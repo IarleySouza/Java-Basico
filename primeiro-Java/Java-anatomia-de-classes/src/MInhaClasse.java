76eebb9004ba8becf6d85cac0ee77a33adff6877
iarley <iarleysouza1003@gmail.com>
2024-05-30 02:20:42 -0300

    Projeto de media

---
+++ primeiro-Java/Java-anatomia-de-classes/src/MInhaClasse.java
@@ -0,0 +1,15 @@
+public class MInhaClasse {
+
+public static void main (String [] args) {
+    String meuNome = "iarley";
+    int numero = 24;
+    boolean verdadeira = true;
+
+    numero = 2004;
+
+
+    System.out.println(numero);
+
+  }
+    
+}
